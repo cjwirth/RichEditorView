67f8641f0710b9efe9510d1c1bf75909c80d4d0d
Caesar Wirth <cjwirth@gmail.com>
2015-04-07 20:37:00 +0900

    Add framework project for Carthage support

---
+++ RichEditorView/RichEditorView-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  RichEditorView-Bridging-Header.h
+//  RichEditorView
+//
+//  Created by Caesar Wirth on 4/7/15.
+//
+//
+
+#ifndef RichEditorView_RichEditorView_Bridging_Header_h
+#define RichEditorView_RichEditorView_Bridging_Header_h
+
+#import "CJWWebView+HackishAccessoryHiding.h"
+
+#endif
---
+++ RichEditorView/RichEditorView.h
@@ -0,0 +1,21 @@
+//
+//  RichEditorView.h
+//  RichEditorView
+//
+//  Created by Caesar Wirth on 4/7/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import <RichEditorView/CJWWebView+HackishAccessoryHiding.h>
+
+//! Project version number for RichEditorView.
+FOUNDATION_EXPORT double RichEditorViewVersionNumber;
+
+//! Project version string for RichEditorView.
+FOUNDATION_EXPORT const unsigned char RichEditorViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RichEditorView/PublicHeader.h>
+
+
