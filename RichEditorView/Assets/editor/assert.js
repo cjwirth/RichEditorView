c536d642577a094ee6c16648be3f718298f49b00
Caesar Wirth <cjwirth@gmail.com>
2016-03-11 23:56:05 +0900

    Ran JS files through beautifier

--- RichEditorView/Assets/editor/assert.js
+++ RichEditorView/Assets/editor/assert.js
@@ -1,25 +1,27 @@
 //worlds smallest unit testing framework
 var Assert = (Assert = function() {
-  var AssertException = function(message) { this.message = message; };
+    var AssertException = function(message) {
+        this.message = message;
+    };
 
-  AssertException.prototype.toString = function () {
-    return 'AssertException: ' + this.message;
-  };
+    AssertException.prototype.toString = function() {
+        return 'AssertException: ' + this.message;
+    };
 
-  this.assert = this.that = function(exp, message) {
+    this.assert = this.that = function(exp, message) {
 
-    if (!exp) {
-      throw new AssertException(message);
+        if (!exp) {
+            throw new AssertException(message);
+        }
     }
-  }
 
-  this.equals = function(expected, actual, detail) {
-    this.assert(expected == actual, 'Failed asserting that ' + expected + '===' + actual + ' :: ' + detail);
-  }
+    this.equals = function(expected, actual, detail) {
+        this.assert(expected == actual, 'Failed asserting that ' + expected + '===' + actual + ' :: ' + detail);
+    }
 
-  this.type = function(expectedType, actual) {
-    this.assert(expectedType ===  actual.constructor.name, 'Failed asserting that types match: ' + expectedType + ' ===  ' + actual.constructor.name);
-  }
-  //end worlds smallest unit testing framework
-  return this;
- }());
+    this.type = function(expectedType, actual) {
+            this.assert(expectedType === actual.constructor.name, 'Failed asserting that types match: ' + expectedType + ' ===  ' + actual.constructor.name);
+        }
+        //end worlds smallest unit testing framework
+    return this;
+}());
--- RichEditorView/Assets/editor/rich_editor.js
+++ RichEditorView/Assets/editor/rich_editor.js
@@ -13,20 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-"use strict";
+ "use strict";
+
+var RE = {};
 
 window.onload = function() {
     RE.callback("ready");
 };
 
-var RE = {};
-
-RE.currentSelection;
-
 RE.editor = document.getElementById('editor');
 
 // Not universally supported, but seems to work in iOS 7 and 8
-document.addEventListener("selectionchange", function() { RE.backuprange(); });
+document.addEventListener("selectionchange", function() {
+    RE.backuprange();
+});
 
 //looks specifically for a Range selection and not a Caret selection
 RE.rangeSelectionExists = function() {
@@ -64,46 +64,46 @@ RE.editor.addEventListener("blur", function() {
 
 RE.customAction = function(action) {
     RE.callback("action/" + action);
-}
+};
 
 RE.callbackQueue = [];
 RE.runCallbackQueue = function() {
-    if (RE.callbackQueue.length == 0) {
+    if (RE.callbackQueue.length === 0) {
         return;
     }
 
     setTimeout(function() {
         window.location.href = "re-callback://";
     }, 0);
-}
+};
 
 RE.getCommandQueue = function() {
     var commands = JSON.stringify(RE.callbackQueue);
     RE.callbackQueue = [];
     return commands;
-}
+};
 
 RE.callback = function(method) {
     RE.callbackQueue.push(method);
     RE.runCallbackQueue();
-}
+};
 
 RE.setHtml = function(contents) {
     RE.editor.innerHTML = contents;
     RE.updatePlaceholder();
-}
+};
 
 RE.getHtml = function() {
     return RE.editor.innerHTML;
-}
+};
 
 RE.getText = function() {
     return RE.editor.innerText;
-}
+};
 
 RE.setPlaceholderText = function(text) {
     RE.editor.setAttribute("placeholder", text);
-}
+};
 
 RE.updatePlaceholder = function() {
     if (RE.editor.textContent.length > 0) {
@@ -111,121 +111,121 @@ RE.updatePlaceholder = function() {
     } else {
         RE.editor.classList.add("placeholder");
     }
-}
+};
 
 RE.removeFormat = function() {
     document.execCommand('removeFormat', false, null);
-}
+};
 
 RE.setFontSize = function(size) {
     RE.editor.style.fontSize = size;
-}
+};
 
 RE.setBackgroundColor = function(color) {
     RE.editor.style.backgroundColor = color;
-}
+};
 
 RE.setHeight = function(size) {
     RE.editor.style.height = size;
-}
+};
 
 RE.undo = function() {
     document.execCommand('undo', false, null);
-}
+};
 
 RE.redo = function() {
     document.execCommand('redo', false, null);
-}
+};
 
 RE.setBold = function() {
     document.execCommand('bold', false, null);
-}
+};
 
 RE.setItalic = function() {
     document.execCommand('italic', false, null);
-}
+};
 
 RE.setSubscript = function() {
     document.execCommand('subscript', false, null);
-}
+};
 
 RE.setSuperscript = function() {
     document.execCommand('superscript', false, null);
-}
+};
 
 RE.setStrikeThrough = function() {
     document.execCommand('strikeThrough', false, null);
-}
+};
 
 RE.setUnderline = function() {
     document.execCommand('underline', false, null);
-}
+};
 
 RE.setTextColor = function(color) {
     RE.restorerange();
     document.execCommand("styleWithCSS", null, true);
     document.execCommand('foreColor', false, color);
     document.execCommand("styleWithCSS", null, false);
-}
+};
 
 RE.setTextBackgroundColor = function(color) {
     RE.restorerange();
     document.execCommand("styleWithCSS", null, true);
     document.execCommand('hiliteColor', false, color);
     document.execCommand("styleWithCSS", null, false);
-}
+};
 
 RE.setHeading = function(heading) {
-    document.execCommand('formatBlock', false, '<h'+heading+'>');
-}
+    document.execCommand('formatBlock', false, '<h' + heading + '>');
+};
 
 RE.setIndent = function() {
     document.execCommand('indent', false, null);
-}
+};
 
 RE.setOutdent = function() {
     document.execCommand('outdent', false, null);
-}
+};
 
 RE.setOrderedList = function() {
     document.execCommand('insertOrderedList', false, null);
-}
+};
 
 RE.setUnorderedList = function() {
     document.execCommand('insertUnorderedList', false, null);
-}
+};
 
 RE.setJustifyLeft = function() {
     document.execCommand('justifyLeft', false, null);
-}
+};
 
 RE.setJustifyCenter = function() {
     document.execCommand('justifyCenter', false, null);
-}
+};
 
 RE.setJustifyRight = function() {
     document.execCommand('justifyRight', false, null);
-}
+};
 
 RE.insertImage = function(url, alt) {
     var html = '<img src="' + url + '" alt="' + alt + '" />';
     RE.insertHTML(html);
     RE.callback("input");
-}
+};
 
 RE.setBlockquote = function() {
     document.execCommand('formatBlock', false, '<blockquote>');
-}
+};
 
 RE.insertHTML = function(html) {
     RE.restorerange();
     document.execCommand('insertHTML', false, html);
-}
+};
 
 RE.insertLink = function(url, title) {
     RE.restorerange();
     var sel = document.getSelection();
-    if (sel.toString().length != 0) {
+    if (sel.toString().length !== 0) {
         if (sel.rangeCount) {
 
             var el = document.createElement("a");
@@ -239,13 +239,13 @@ RE.insertLink = function(url, title) {
         }
     }
     RE.callback("input");
-}
+};
 
 RE.prepareInsert = function() {
     RE.backuprange();
-}
+};
 
-RE.backuprange = function(){
+RE.backuprange = function() {
     var selection = window.getSelection();
     if (selection.rangeCount > 0) {
         var range = selection.getRangeAt(0);
@@ -253,34 +253,35 @@ RE.backuprange = function(){
             "startContainer": range.startContainer,
             "startOffset": range.startOffset,
             "endContainer": range.endContainer,
-            "endOffset": range.endOffset};
+            "endOffset": range.endOffset
+        };
     }
-}
+};
 
 RE.addRangeToSelection = function(selection, range) {
-  if (selection) {
-    selection.removeAllRanges();
-    selection.addRange(range);
-  }
-}
+    if (selection) {
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+};
 
 // Programatically select a DOM element
 RE.selectElementContents = function(el) {
-  var range = document.createRange();
-  range.selectNodeContents(el);
-  var sel = window.getSelection();
-  // this.createSelectionFromRange sel, range
-  RE.addRangeToSelection(sel, range);
-}
+    var range = document.createRange();
+    range.selectNodeContents(el);
+    var sel = window.getSelection();
+    // this.createSelectionFromRange sel, range
+    RE.addRangeToSelection(sel, range);
+};
 
-RE.restorerange = function(){
+RE.restorerange = function() {
     var selection = window.getSelection();
     selection.removeAllRanges();
     var range = document.createRange();
     range.setStart(RE.currentSelection.startContainer, RE.currentSelection.startOffset);
     range.setEnd(RE.currentSelection.endContainer, RE.currentSelection.endOffset);
     selection.addRange(range);
-}
+};
 
 RE.focus = function() {
     var range = document.createRange();
@@ -290,47 +291,46 @@ RE.focus = function() {
     selection.removeAllRanges();
     selection.addRange(range);
     RE.editor.focus();
-}
+};
 
 RE.blurFocus = function() {
     RE.editor.blur();
-}
+};
 
 /**
 Recursively search element ancestors to find a element nodeName e.g. A
 **/
-var _findNodeByNameInContainer  = function(element, nodeName, rootElementId) {
-  if (element.nodeName == nodeName) {
-    return element
-  } else {
-    if (element.id === rootElementId) {
-      // console.log('Root content node reached!')
-      return null
+var _findNodeByNameInContainer = function(element, nodeName, rootElementId) {
+    if (element.nodeName == nodeName) {
+        return element;
+    } else {
+        if (element.id === rootElementId) {
+            return null;
+        }
+        _findNodeByNameInContainer(element.parentElement, nodeName, rootElementId);
     }
-    // console.log('Recursing...')
-    _findNodeByNameInContainer(element.parentElement, nodeName, rootElementId)
-  }
-}
+};
 
 var isAnchorNode = function(node) {
-  return ("A" == node.nodeName)
-}
+    return ("A" == node.nodeName);
+};
 
 RE.getAnchorTagsInNode = function(node) {
-  var links = [];
+    var links = [];
 
-  while (node.nextSibling != null && node.nextSibling != undefined) {
-    node = node.nextSibling;
-    if (isAnchorNode(node)) {
-      links.push(node.getAttribute('href'))
+    while (node.nextSibling !== null && node.nextSibling !== undefined) {
+        node = node.nextSibling;
+        if (isAnchorNode(node)) {
+            links.push(node.getAttribute('href'));
+        }
     }
-  }
-  return links;
-}
+    return links;
+};
 
 RE.countAnchorTagsInNode = function(node) {
-  return RE.getAnchorTagsInNode(node).length;
-}
+    return RE.getAnchorTagsInNode(node).length;
+};
+
 /**
  * If the current selection's parent is an anchor tag, get the href.
  * @returns {string}
@@ -338,20 +338,20 @@ RE.countAnchorTagsInNode = function(node) {
 RE.getSelectedHref = function() {
     var href, sel;
     href = '';
-    sel = getSelection()
+    sel = window.getSelection();
     if (!RE.rangeOrCaretSelectionExists()) {
-      return null;
+        return null;
     }
 
-    let tags = RE.getAnchorTagsInNode(sel.anchorNode)
+    var tags = RE.getAnchorTagsInNode(sel.anchorNode);
     //if more than one link is there, return null
     if (tags.length > 1) {
-      return null;
+        return null;
     } else if (tags.length == 1) {
-      href = tags[0]
+        href = tags[0];
     } else {
-      href = _findNodeByNameInContainer(sel.anchorNode.parentElement, 'A', 'editor')
+        href = _findNodeByNameInContainer(sel.anchorNode.parentElement, 'A', 'editor');
     }
 
-    return href ? href : null
-}
+    return href ? href : null;
+};
